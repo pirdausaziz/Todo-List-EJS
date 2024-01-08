d7ab4329c787deb056fd0803899888ddfdeb6193
pirdausaziz <pirdausaziz@gmail.com>
2024-01-08 14:17:02 +0800

    removed redundant file

--- app.js
+++ app.js
@@ -1,53 +1,16 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-// const mongoose = require("mongoose");
-const _ = require("lodash"); //customize letters
-
+const _ = require("lodash");
 const date = require(__dirname + "/date.js");
-
 const app = express();
 
 var ListItem = [];
-//let work_items = [];
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
-
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-
-// mongoose.connect("mongodb://localhost:27017/todolistDB", {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-// });
-
-// const itemsSchema = {
-//     name: String
-// };
-
-//const Item = mongoose.model("Item", itemsSchema);
-
-// const item1 = new Item({
-//     name: "Welcome to your Todolist"
-// });
-
-// const item2 = new Item({
-//     name: "Hit + button to add a new item"
-// });
-
-// const item3 = new Item({
-//     name: "<----Hit this Button to delete new item"
-// });
-
-// const defaultItems = [item1, item2, item3];
-
-// const listSchema = {
-//     name: String,
-//     items: [itemsSchema]
-// };
-
-//const List = mongoose.model("List", listSchema);
 
 app.get("/", (req, res) => {
 
